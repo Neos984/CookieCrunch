e34f61d5cc6cef7e267cb727e71f374470f61523
Tairi Buchanan <tairibuchanan@gmail.com>
2014-11-09 20:44:26 +0800

    Ready, Set...

---
+++ Set.swift
@@ -0,0 +1,53 @@
+//
+//  Set.swift
+//  CookieCrunch
+//
+//  Created by Tairi Buchanan on 11/9/14.
+//  Copyright (c) 2014 Tairi Buchanan. All rights reserved.
+//
+
+struct Set<T: Hashable>: SequenceType, Printable {
+    private var dictionary = Dictionary<T, Bool>()
+    
+    mutating func addElement(newElement: T) {
+        dictionary[newElement] = true
+    }
+    
+    mutating func removeElement(element: T) {
+        dictionary[element] = nil
+    }
+    
+    func containsElement(element: T) -> Bool {
+        return dictionary[element] != nil
+    }
+    
+    func allElements() -> [T] {
+        return Array(dictionary.keys)
+    }
+    
+    var count: Int {
+        return dictionary.count
+    }
+    
+    func unionSet(otherSet: Set<T>) -> Set<T> {
+        var combined = Set<T>()
+        
+        for obj in dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+        
+        for obj in otherSet.dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+        
+        return combined
+    }
+    
+    func generate() -> IndexingGenerator<Array<T>> {
+        return allElements().generate()
+    }
+    
+    var description: String {
+        return dictionary.description
+    }
+}
