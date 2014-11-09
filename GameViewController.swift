b27ec34ad77f19fdeb6c915f9fcf9966bf38b06a
Tairi Buchanan <tairibuchanan@gmail.com>
2014-11-09 21:33:19 +0800

    Loading Levels from JSON Files

--- GameViewController.swift
+++ GameViewController.swift
@@ -35,7 +35,7 @@ class GameViewController: UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
-        level = Level()
+        level = Level(filename: "Level_3")
         scene.level = level
         
         // Present the scene.
---
+++ Tile.swift
@@ -0,0 +1,11 @@
+//
+//  Tile.swift
+//  CookieCrunch
+//
+//  Created by Tairi Buchanan on 11/9/14.
+//  Copyright (c) 2014 Tairi Buchanan. All rights reserved.
+//
+
+class Tile{
+    
+}
